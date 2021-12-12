aba24b3dbead4313f59eef5c4d5076a48bfedcd6
Tim <tcarthew@gmail.com>
2021-12-12 19:25:57 +0200

    Update frontend for deployed api

--- project/udagram-frontend/src/environments/environment.ts
+++ project/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
     production: false,
     appName: 'Udagram',
-    apiHost: 'http://localhost:8080/api/v0'
+    apiHost: 'http://a3ea0e37808f54223a0524a7f3ba66f1-730505508.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
